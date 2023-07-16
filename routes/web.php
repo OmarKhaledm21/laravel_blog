1ff7b0d55d656111f2b5fbd50ae36c4057139425
Omarkhaledm21 <omarkhaledm21@gmail.com>
2023-07-16 11:30:41 +0300

    REMOVE: remomved commented code from web.php routes

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,6 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
 Route::redirect('/', 'posts');
 Route::get('/posts', function () {
     return view('posts', [
@@ -42,18 +30,3 @@ Route::get('categories/{category}', function (Category $category) {
 
 
 
-
-    // if (!file_exists($$path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-    //     // ddd('file doesnot exist');
-    //     // abort(404,'File not found');
-    //     return redirect('/');
-    // }
-
-    // $post  = cache()->remember("posts.{$slug}", now()->addSeconds(5), function () use ($path) {
-    //     var_dump('file get contents');
-    //     return  file_get_contents($path);
-    // });
-
-    // return view('post', [
-    //     'post' => $post
-    // ]);
